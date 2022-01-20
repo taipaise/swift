cde8e24c94ac168bdb86dbf5f25332c5940a6b0d
taipaise <taipaise@gmail.com>
2022-01-20 22:16:36 +0900

    first tutorial

---
+++ ex01.playground/Contents.swift
@@ -0,0 +1,9 @@
+
+import UIKit // ios 기본 라이브러리를 포함한다
+
+var greeting = "Hello, playground" //세미콜론은 있어도 되고 없어도 된다. 대부분은 넣지 않는 편이다.
+// 스위프트에서 기본 출력물은 Print이다.
+print(greeting)
+print("Swift tutorial")
+//print('hello')는 오류. 스위프트는 반따옴표는 지원하지 않기 때문이다.
+
